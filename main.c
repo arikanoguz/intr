169e24ac041e43ed2b1c2e420a470285b3023a4b
arikanoguz <44438831+arikanoguz@users.noreply.github.com>
2019-09-23 16:16:01 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,14 +2,18 @@
 #include <stdlib.h>
 
 int main() {
-	int var1, var2, var3, sum, avg, prd;
-	printf("Input three integers:");
-	scanf("%d %d %d" , &var1, &var2, &var3);
-	sum = var1 + var2 + var3;
-	avg = sum/3;
-	prd = var1 * var2 * var3;
-	printf("Sum is %d\n", sum);
-	printf("Average is %d\n", avg);
-	printf("Product is %d\n", prd);
+	int var1, var2, var3, var4, sum, avg, prd;
+	printf("Input four integers:");
+	scanf("%d %d %d %d" , &var1, &var2, &var3, &var4);
+	if (var1 <= 0)
+	var1 = 1;
+	if (var2 <= 0)
+	var2 = 1;
+	if (var3 <= 0)
+	var3 = 1;
+	if (var4 <= 0)
+	var4 = 1;
+	prd = var1 * var2 *var3 * var4;
+	printf("%d", prd);
 return 0;
 }
