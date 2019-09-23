ed2563a4136a5f88e45dcb048899a3dc321d45f9
arikanoguz <44438831+arikanoguz@users.noreply.github.com>
2019-09-23 15:38:57 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 
 int main() {
-	int var1, var2;
+	int var1, var2, remainder, r2;
 	printf("Input two integers:");
 	scanf("%d", &var1);
 	scanf("%d", &var2);
-	if (var1 > var2)
-		printf("%d is larger", var1);
-		else if (var2 > var1)
-			printf("%d is larger", var2);
+	remainder = var1 % var2;
+	if (remainder == 0)
+		printf("First is a multiple of the second");
 			else 
-				printf("These numbers are equal");
+				;
+	r2 = var2 % var1;
+	if (r2 == 0)
+		printf("Second is a multiple of the first");
 return 0;
 }
